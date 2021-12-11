59f611da780dc3c4c879c112d4a3b96f149ab4b7
Ravi <ravi4uininin@gmail.com>
2021-12-11 19:52:33 +0530

    Property card changes

--- src/app/property/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
 selector:'app-property-card',
@@ -10,10 +10,6 @@ styleUrls:['property-card.component.css']
 
 export class PropertyCardComponent
 {
-  Property: any={
-    "Id":1,
-    "Name":"Rhythm's Home",
-    "Type":"Bunglow",
-    "Price":20000000
-  }
+
+  @Input() Property:any
 }
